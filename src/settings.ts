988531b4e1af4071f1b446d8434eefe081db7683
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-05-17 11:17:38 +0100

    refactor: move userDir to root

--- src/settings.ts
+++ src/settings.ts
@@ -8,8 +8,8 @@ const cwd = dirname(fileURLToPath(import.meta.url));
 export const settings: LocalSettings = {
   httpAdminRoot: "/",
   httpNodeRoot: "/api",
-  userDir: resolve(cwd, "userDir"),
-  nodesDir: resolve(cwd, "userDir", "nodes"),
+  userDir: resolve(cwd, ".."),
+  nodesDir: resolve(cwd, "..", "nodes"),
   functionGlobalContext: {},
   uiHost: "0.0.0.0",
   uiPort: parseInt(process.env.PORT) || 8080,
