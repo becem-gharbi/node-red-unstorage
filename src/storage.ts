2b14d08cd31d52d5b8f860b7811d2bddcaa17f80
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-05-17 15:00:23 +0100

    refactor: simplify library entry storage handlers

--- src/storage.ts
+++ src/storage.ts
@@ -1,12 +1,12 @@
 //@ts-nocheck
 import type { StorageModule } from "@node-red/runtime";
 import { createStorage, prefixStorage, Storage } from "unstorage";
-import Library from "./models/library.js";
 import fsDriver from "unstorage/drivers/fs";
 
 var appname: string;
 
-export var appStorage: Storage;
+var appStorage: Storage;
+var libraryStorage: Storage;
 
 export const storageModule: StorageModule = {
   init: () => {
@@ -17,6 +17,8 @@ export const storageModule: StorageModule = {
     });
 
     appStorage = prefixStorage(unstorage, appname);
+
+    libraryStorage = prefixStorage(appStorage, "library");
   },
 
   getFlows: () => appStorage.getItem("flows").then((flows) => flows || []),
@@ -26,90 +28,69 @@ export const storageModule: StorageModule = {
   getCredentials: () =>
     appStorage.getItem("credentials").then((creds) => creds || {}),
 
-  saveCredentials: () => appStorage.setItem("credentials", credentials),
+  saveCredentials: (credentials) =>
+    appStorage.setItem("credentials", credentials),
 
   getSettings: () =>
     appStorage.getItem("settings").then((settings) => settings || {}),
 
-  saveSettings: () => appStorage.setItem("settings", settings),
+  saveSettings: (settings) => appStorage.setItem("settings", settings),
 
   getSessions: () =>
     appStorage.getItem("sessions").then((sessions) => sessions || {}),
 
-  saveSessions: () => appStorage.setItem("sessions", sessions),
+  saveSessions: (sessions) => appStorage.setItem("sessions", sessions),
 
-  getLibraryEntry: function (type, name) {
-    console.log("********* getLibraryEntry *********");
-
+  getLibraryEntry: async function (type, name) {
     if (name == "") {
       name = "/";
     } else if (name.substr(0, 1) != "/") {
       name = "/" + name;
     }
 
-    return new Promise(function (resolve, reject) {
-      Library.findOne(
-        { appname: appname, type: type, name: name },
-        function (err, file) {
-          if (err) {
-            reject(err);
-          } else if (file) {
-            resolve(file.body);
-          } else {
-            var reg = new RegExp("^" + name, "");
-            Library.find(
-              { appname: appname, type: type, name: reg },
-              function (err, fileList) {
-                if (err) {
-                  reject(err);
-                } else {
-                  var dirs = [];
-                  var files = [];
+    const library = await libraryStorage.getItem(name);
 
-                  for (var i = 0; i < fileList.length; i++) {
-                    var n = fileList[i].name;
-                    n = n.replace(name, "");
-                    if (n.indexOf("/") == -1) {
-                      var f = fileList[i].meta;
-                      f.fn = n;
-                      files.push(f);
-                    } else {
-                      n = n.substr(0, n.indexOf("/"));
-                      dirs.push(n);
-                    }
-                  }
-                  dirs = dirs.concat(files);
-                  resolve(dirs);
-                }
-              }
-            );
-          }
-        }
-      );
-    });
+    if (library) {
+      const parsedLibrary = JSON.parse(library);
+
+      return parsedLibrary.body;
+    }
+
+    const libraryKeys = await libraryStorage.getKeys(name);
+
+    var dirs = [];
+    var files = [];
+
+    for (var i = 0; i < libraryKeys.length; i++) {
+      const library = await libraryStorage.getItem(libraryKeys[i]);
+
+      const parsedLibrary = JSON.parse(library);
+
+      var n = parsedLibrary.name;
+      n = n.replace(name, "");
+      if (n.indexOf("/") == -1) {
+        var f = parsedLibrary.meta;
+        f.fn = n;
+        files.push(f);
+      } else {
+        n = n.substr(0, n.indexOf("/"));
+        dirs.push(n);
+      }
+    }
+
+    dirs = dirs.concat(files);
+
+    return dirs;
   },
 
   saveLibraryEntry: function (type, name, meta, body) {
-    console.log("********* saveLibraryEntry *********");
+    var p = name.split("/"); // strip multiple slash
+    p = p.filter(Boolean);
+    name = p.slice(0, p.length).join("/");
+    if (name != "" && name.substr(0, 1) != "/") {
+      name = "/" + name;
+    }
 
-    return new Promise(function (resolve, reject) {
-      var p = name.split("/"); // strip multiple slash
-      p = p.filter(Boolean);
-      name = p.slice(0, p.length).join("/");
-      if (name != "" && name.substr(0, 1) != "/") {
-        name = "/" + name;
-      }
-      Library.findOneAndUpdate(
-        { appname: appname, name: name },
-        { name: name, meta: meta, body: body, type: type },
-        function (err, library) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        }
-      );
-    });
+    return libraryStorage.setItem(name, { name, meta, body, type });
   },
 };
